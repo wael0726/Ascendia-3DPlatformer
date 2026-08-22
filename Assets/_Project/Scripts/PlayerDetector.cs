61845a7a077f7b4d336ba8223c82a76843861ca8
Bennabi Malek Wael <153573632+wael0726@users.noreply.github.com>
2026-08-22 02:39:26 -0400

    Enhance PlayerDetector with obstacle detection
    Added obstacleMask to PlayerDetector for improved detection strategy.

--- Assets/_Project/Scripts/PlayerDetector.cs
+++ Assets/_Project/Scripts/PlayerDetector.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 using Utilities;
-
 namespace Platformer {
     public class PlayerDetector : MonoBehaviour {
-        [SerializeField] float detectionAngle = 60f; // Cone in front of enemy
-        [SerializeField] float detectionRadius = 8f; // Large circle around enemy
-        [SerializeField] float innerDetectionRadius = 1f; // Small circle around enemy
-        [SerializeField] float detectionCooldown = 0.1f; // Time between detections
-        [SerializeField] float attackRange = 2f; // Distance from enemy to player to attack
+        [SerializeField] float detectionAngle = 60f;
+        [SerializeField] float detectionRadius = 8f;
+        [SerializeField] float innerDetectionRadius = 1f;
+        [SerializeField] float detectionCooldown = 0.1f;
+        [SerializeField] float attackRange = 2f; 
+        [SerializeField] LayerMask obstacleMask;
         
         public Transform Player { get; private set; }
         public Health PlayerHealth { get; private set; }
@@ -15,23 +15,19 @@ namespace Platformer {
         CountdownTimer detectionTimer;
         
         IDetectionStrategy detectionStrategy;
-
         void Awake() {
-            Player = GameObject.FindGameObjectWithTag("Player").transform; // Make sure to TAG the player!
+            Player = GameObject.FindGameObjectWithTag("Player").transform; // Make sure to TAG the player
             PlayerHealth = Player.GetComponent<Health>();
         }
-
         void Start() {
             detectionTimer = new CountdownTimer(detectionCooldown);
-            detectionStrategy = new ConeDetectionStrategy(detectionAngle, detectionRadius, innerDetectionRadius);
+            detectionStrategy = new ConeDetectionStrategy(detectionAngle, detectionRadius, innerDetectionRadius, obstacleMask);
         }
         
         void Update() => detectionTimer.Tick(Time.deltaTime);
-
         public bool CanDetectPlayer() {
             return detectionTimer.IsRunning || detectionStrategy.Execute(Player, transform, detectionTimer);
         }
-
         public bool CanAttackPlayer() {
             var directionToPlayer = Player.position - transform.position;
             return directionToPlayer.magnitude <= attackRange;
@@ -41,15 +37,12 @@ namespace Platformer {
         
         void OnDrawGizmos() {
             Gizmos.color = Color.red;
-
             // Draw a spheres for the radii
             Gizmos.DrawWireSphere(transform.position, detectionRadius);
             Gizmos.DrawWireSphere(transform.position, innerDetectionRadius);
-
             // Calculate our cone directions
             Vector3 forwardConeDirection = Quaternion.Euler(0, detectionAngle / 2, 0) * transform.forward * detectionRadius;
             Vector3 backwardConeDirection = Quaternion.Euler(0, -detectionAngle / 2, 0) * transform.forward * detectionRadius;
-
             // Draw lines to represent the cone
             Gizmos.DrawLine(transform.position, transform.position + forwardConeDirection);
             Gizmos.DrawLine(transform.position, transform.position + backwardConeDirection);
